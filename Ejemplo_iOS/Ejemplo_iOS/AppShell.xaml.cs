91819d03b60327c714a2febc80bebdce1a54e140
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-02-04 20:40:35 -0300

    inicio repositorio

---
+++ Ejemplo_iOS/Ejemplo_iOS/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Ejemplo_iOS.Pages;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Ejemplo_iOS/Ejemplo_iOS/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ejemplo_iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
